ee118b586bbccb9c3ac2f136a2c1a415ea762937
Jasper <japsai@gmail.com>
2016-05-13 14:50:21 -0700

    Got event durations to work'

--- ids_expyfun_alexis.py
+++ ids_expyfun_alexis.py
@@ -5,80 +5,104 @@
 #          simplified bsd-3 license
 
 """Script for infant basic preference testing using auditory stimuli"""
-
 import numpy as np
 from os import path as op
 from expyfun import ExperimentController, get_keyboard_input
 from expyfun.stimuli import read_wav
 from expyfun._trigger_controllers import decimals_to_binary
-from expyfun import assert_version
 import expyfun.analyze as ea
+from scipy.misc import imread
 from expyfun.visual import RawImage
 
+def matchPressReleaseEvents(events):
+    """Assumes events in chron. order"""
+    lastPressByKey = {}
+    durations = []
+    for event in events:
+        key, etime, pressOrRelease = event
+        if pressOrRelease == 'press':
+            assert not key in lastPressByKey, "two presses of same key"
+            lastPressByKey[key] = etime
+        elif pressOrRelease == 'release':
+            if key in lastPressByKey:
+                pressTime = lastPressByKey[key]
+                del lastPressByKey[key]
+            else:
+                """No press found for this release. Count from beginning"""
+                pressTime = 0
+            durations.append((key, etime-pressTime))
+    return durations
+
 fs= 24414
-stim_dir = op.join(op.dirname(__file__), 'words')
-sound_files = ['ko_ko_sa_250ms.wav']
+#stim_dir = op.join(op.dirname(__file__), 'words')
+#sound_files = ['ko_ko_sa_250ms.wav']
 
-#from expyfun import ExperimentController, get_keyboard_input#, set_log_level
-#from expyfun.io import read_hdf5
-#import expyfun.analyze as ea
-#from expyfun.visual import RawImage
-
-#     for ii, wav in enumerate(wavs):
-
-
- # define usable buttons / keys
-    #live_keys = [x + 1 for x in range(num_freqs)]
-
-print(__doc__)                        
-from scipy.misc import imread
-
-#from scipy.misc import imread
-
-#assert_version('8511a4d')
-
-sound_files = {j: op.join(stim_dir, k)
-               for j, k in enumerate(sound_files)}
-wavs = [np.ascontiguousarray(read_wav(v)) for _, v in sorted(sound_files.items())]
-wav = sound_files[0]
+#sound_files = {j: op.join(stim_dir, k)
+#               for j, k in enumerate(sound_files)}
+#wavs = [np.ascontiguousarray(read_wav(v)) for _, v in sorted(sound_files.items())]
+#wav = sound_files[0]
 # convert length of wave files into number of bits
-n_bits = int(np.floor(np.log2(len(wavs)))) + 1
-with ExperimentController('IDS', stim_db=75, stim_fs=fs, stim_rms=0.01,
-                          check_rms=None, suppress_resamp=True, version='dev') as ec:
-                              
-               
+#n_bits = int(np.floor(np.log2(len(wavs)))) + 1
+#stim_len = 1./fs * len(wav[0][0])  # in seconds
+stimdir = 'stimuli'
+imgs = [op.join(stimdir, i) for i in ['circle.png','star.png']]
+rewardfpath = op.join(stimdir, 'like.png')
+punishfpath = op.join(stimdir, 'fail.png')
 
-            
-    # stamp trigger line prior to stimulus onset
+
+with ExperimentController('testExp', participant='foo', session='001',
+                          output_dir='.', version='dev') as ec:
+
+    ec.screen_prompt('Ready?', max_wait=1)
+
     #ec.clear_buffer()
-    #ec.load_buffer(wav[0])
-    #ec.identify_trial(ec_id=str(ii), ttl_id=decimals_to_binary([ii], [n_bits]))
-    # our next start time is our last start time, plus
-    # the stimulus duration
-    #stim_len = 1./fs * len(wav[0][0])  # in seconds
-    ec.start_stimulus()  # stamps stimulus onset
-    ec.identify_trial(ec_id='one-tone trial')
+    #ec.load_buffer(tone)
+    #dot.draw()
+    #ec.identify_trial(ec_id='xyz', ttl_id=[0, 0])
     ec.listen_presses()
-    ec.load_buffer(read_wav(wav)[0])
-    ec.play()
-    ec.wait_secs(3)
-    ec.stop()
-    
-    #ec.flip()
-    im = imread("/Users/bossler/Pictures/animals/Slide01.jpg")
-    myImg = RawImage(ec, im)
-    myImg.draw()
 
-    #ec.write_data_line('listen / while / get_presses', events)
-    #for (k,s,t) in events:
-    #    print(k,s,t)
-#    ec.flip()  # clears the screen
+    ## stimulus phase
+    for s in [0,1,0]:
+        #ec.start_stimulus()
+        RawImage(ec, imread(imgs[s])).draw()
+        ec.flip()
+        ec.wait_secs(.3)
+        #ec.stop()
+    ec.get_presses(kind='both') # Sends responses during stimuli to log.
+    ec.listen_presses() # Clear response buffer at start of response phase.
+
+    ## response phase
+    ec.flip()
+    ec.wait_secs(2)
+
+    ## analyse responses
+    events = ec.get_presses(kind='both')
+    keydurs = matchPressReleaseEvents(events)
+    print(keydurs)
+    if not len(events):
+        message = 'no keys pressed'
+    else:
+        message = ['{} {} after {} secs\n'
+                   ''.format(k, r, round(t, 4)) for k, t, r in events]
+        message = ''.join(message)
+    ec.screen_prompt(message, 3)
+
+    ## reward phase
+    if False:
+        RawImage(ec, imread(rewardfpath)).draw()
+    else:
+        RawImage(ec, imread(punishfpath)).draw()
+    ec.flip()
+    ec.wait_secs(2)
+
+
+    #ec.trial_ok()
+    #print('Presses:\n{}'.format(presses))
+    ec.check_force_quit()  # make sure we're not trying to quit
+
+
         
     
-    ec.wait_secs(3)  # wait through tone duration to stop the playback
-    #ec.stop()
-    #ec.trial_ok()
-    events = ec.get_presses(kind='both')
-    ec.check_force_quit()  # make sure we're not trying to quit
+    
     
     
